1815bcc0e16127d6e21fef7d863d75edfbcfc50f
sakiyamaK <sakiyama.k@gmail.com>
2020-10-19 11:51:04 +0900

    add swiftyattribute

---
+++ SampleLibiraries/SwiftyAttribute/SwiftyAttributeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftyAttributeViewController.swift
+//  SampleLibiraries
+//
+//  Created by sakiyamaK on 2020/10/19.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import Foundation
