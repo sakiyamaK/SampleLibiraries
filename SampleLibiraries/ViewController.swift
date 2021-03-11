27dfe1672dcbcb5ff9e5ed29e34c89e976a15a3d
sakiyamaK <sakiyama.k@gmail.com>
2021-03-11 16:49:03 +0900

    add setting

--- SampleLibiraries/ViewController.swift
+++ SampleLibiraries/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var licenseButton: UIButton! {
+        didSet {
+            licenseButton.addTarget(self, action: #selector(tapLicenseButton), for: .touchUpInside)
+        }
+    }
+
+    @objc func tapLicenseButton() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
